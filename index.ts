17111e8ac1f10cb3e5904effaee504a01f722eac
kien <dp1.1a2kien@gmail.com>
2024-12-12 17:12:30 +0700

    pug

--- index.ts
+++ index.ts
@@ -1,8 +1,10 @@
 import express, { Express, Request, Response } from "express";
 const app: Express = express();
 const port: number = 3000;
+app.set("views", "./views");
+app.set("view engine", "pug");
 app.get("/topic", (req: Request, res: Response) => {
-  res.send("chủ đề bài hát");
+  res.render("client/page/topic/index");
 });
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
