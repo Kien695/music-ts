cd0c2ceaeccda4108d7380612a1b4e806a0489ee
kien <dp1.1a2kien@gmail.com>
2024-12-19 17:56:21 +0700

    topics

--- controller/admin/topic.controller.ts
+++ controller/admin/topic.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import Topic from "../../models/topic.model";
+import { systemConfig } from "../../config/config";
 //[get]/admin/topics
 export const index = async (req: Request, res: Response) => {
   const topics = await Topic.find({
@@ -9,4 +10,68 @@ export const index = async (req: Request, res: Response) => {
     pageTitle: "Quản lý chủ đề",
     topics: topics,
   });
+};
+//[get]/admin/topics/create
+export const create = async (req: Request, res: Response) => {
+  res.render("admin/page/topic/create", {
+    pageTitle: "Trang tạo mới",
+  });
+};
+//[post]/admin/topics/create
+export const createPost = async (req: Request, res: Response) => {
+  const dataSong = {
+    title: req.body.title,
+    description: req.body.description,
+    status: req.body.status,
+    avatar: req.body.avatar,
+  };
+  const topic = new Topic(dataSong);
+  await topic.save();
+  res.redirect(`${systemConfig.prefixAdmin}/topics`);
+};
+//[get]/admin/topics/detail/:id
+export const detail = async (req: Request, res: Response) => {
+  const id = req.params.id;
+  const topic = await Topic.findOne({
+    _id: id,
+  });
+  res.render("admin/page/topic/detail", {
+    pageTitle: "Trang chi tiết",
+    topic: topic,
+  });
+};
+//[get]/admin/topics/detail/:id
+export const edit = async (req: Request, res: Response) => {
+  const id = req.params.id;
+  const topic = await Topic.findOne({
+    _id: id,
+  });
+  res.render("admin/page/topic/edit", {
+    pageTitle: "Trang chỉnh sửa",
+    topic: topic,
+  });
+};
+//[patch]/admin/topics/detail/:id
+export const editPatch = async (req: Request, res: Response) => {
+  const id = req.params.id;
+  const topic = await Topic.updateOne(
+    {
+      _id: id,
+    },
+    req.body
+  );
+  res.redirect("back");
+};
+//[patch]/admin/topics/detail/:id
+export const deleted = async (req: Request, res: Response) => {
+  const id = req.params.id;
+  await Topic.updateOne(
+    {
+      _id: id,
+    },
+    {
+      deleted: true,
+    }
+  );
+  res.redirect("back");
 };
--- router/admin/topic.router.ts
+++ router/admin/topic.router.ts
@@ -1,7 +1,24 @@
 import { Router } from "express";
-
+import multer from "multer";
 import * as controller from "../../controller/admin/topic.controller";
+import * as uploadCloud from "../../middleware/admin/uploadCloud.middleware";
+const upload = multer();
 const router: Router = Router();
 router.get("/", controller.index);
-
+router.get("/create", controller.create);
+router.post(
+  "/create",
+  upload.single("avatar"),
+  uploadCloud.uploadSingle,
+  controller.createPost
+);
+router.get("/detail/:id", controller.detail);
+router.get("/edit/:id", controller.edit);
+router.patch(
+  "/edit/:id",
+  upload.single("avatar"),
+  uploadCloud.uploadSingle,
+  controller.editPatch
+);
+router.patch("/delete/:id", controller.deleted);
 export const topicRouter: Router = router;
