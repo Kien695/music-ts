3dddc59745a27e3ed0886bfcadec9ff127b23676
kien <dp1.1a2kien@gmail.com>
2025-01-04 11:01:16 +0700

    top

--- controller/client/dashboard.controller.ts
+++ controller/client/dashboard.controller.ts
@@ -1,7 +1,36 @@
 import { Request, Response } from "express";
 import Topic from "../../models/topic.model";
+import Song from "../../models/song.model";
+import Singer from "../../models/singer.model";
+import Favorite from "../../models/favorite-song.model";
 export const dashboard = async (req: Request, res: Response) => {
+  const songLikes = await Song.find({
+    deleted: false,
+  })
+    .sort({ like: -1 })
+    .limit(3);
+  for (const song of songLikes) {
+    const infoSinger = await Singer.findOne({
+      _id: song.singerId,
+      deleted: false,
+    });
+    song["infoSinger"] = infoSinger;
+  }
+  const songListens = await Song.find({
+    deleted: false,
+  })
+    .sort({ listen: -1 })
+    .limit(3);
+  for (const song of songListens) {
+    const infoSinger = await Singer.findOne({
+      _id: song.singerId,
+      deleted: false,
+    });
+    song["infoSinger"] = infoSinger;
+  }
   res.render("client/page/dashboard/index", {
     pageTitle: "Trang chủ",
+    songLikes: songLikes,
+    songListens: songListens,
   });
 };
